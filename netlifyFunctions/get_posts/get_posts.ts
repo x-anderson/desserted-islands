3efc4295d6236057d27d9bcc20572538551805f4
Xander Anderson <x.anderson@synthace.com>
2023-04-27 12:30:44 +0100

    Add get_posts

--- netlifyFunctions/get_posts/get_posts.ts
+++ netlifyFunctions/get_posts/get_posts.ts
@@ -1,9 +1,38 @@
-import { Handler } from '@netlify/functions'
+const MongoClient = require("mongodb").MongoClient;
 
-export const handler: Handler = async (event, context) => {
-  const unixTime = Math.floor(Date.now() / 1000);
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(process.env.MONGO_DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const res = await db.collection(process.env.MONGO_DB_COLLECTION).find({}).toArray();
+
   return {
     statusCode: 200,
-    body: `The Unix time is ${unixTime}`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(res),
   };
-}
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+  return queryDatabase(db);
+};
