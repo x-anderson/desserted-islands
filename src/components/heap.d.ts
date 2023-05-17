b9782789d0bd9c1449b982fdeacbe701ac69f5b6
Xander Anderson <x.anderson@synthace.com>
2023-05-17 20:34:26 +0100

    Prototype tracking

---
+++ src/components/heap.d.ts
@@ -0,0 +1,15 @@
+declare var heap: IHeap;
+
+interface IHeap {
+  track: (event: string, properties?: Object) => void;
+  identify: (identity: string) => void;
+  resetIdentity: () => void;
+  addUserProperties: (properties: Object) => void;
+  addEventProperties: (properties: Object) => void;
+  removeEventProperty: (property: string) => void;
+  clearEventProperties: () => void;
+  appid: string;
+  userId: string;
+  identity: string | null;
+  config: any;
+}
