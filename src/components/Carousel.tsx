fd0647de92655722a0bbaaeef2ae4450af85a337
Xander Anderson <x.anderson@synthace.com>
2024-04-28 13:24:10 +0100

    Add meta deta to site

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,34 +6,44 @@ import "./Carousel.css";
 export default function CarouselSection() {
   return (
     <Section about="carousel" placement="odd" className="carousel-section">
-      <Carousel
-        showThumbs={false}
-        autoPlay={true}
-        interval={3000}
-        infiniteLoop
-      >
+      <Carousel showThumbs={false} autoPlay={true} interval={3000} infiniteLoop>
         <div className="carousel-div">
-          <img src="/ar-pineapple.jpg" alt="bolo di anasa" />
+          <img
+            src="/ar-pineapple.jpg"
+            alt="Bolo di Anasa (pineapple upside-down-cake) from Aruba"
+          />
           <p className="legend">🇦🇼 Bolo di Anasa from Aruba</p>
         </div>
         <div>
-          <img src="/in-kue-mangkok.jpg" alt="kue mangkok" />
+          <img
+            src="/in-kue-mangkok.jpg"
+            alt="Kue Mangkok (colorful steamed cakes) from Indonesia"
+          />
           <p className="legend">🇮🇩 Kue Mangkok from Indonesia</p>
         </div>
         <div>
-          <img src="/tv-coconut.jpg" alt="coconut pudding" />
+          <img src="/tv-coconut.jpg" alt="Coconut Pudding from Tuvalu" />
           <p className="legend">🇹🇻 Coconut Pudding from Tuvalu</p>
         </div>
         <div className="carousel-div">
-          <img src="/sv-blotkake.jpg" alt="blotkake" />
+          <img
+            src="/sv-blotkake.jpg"
+            alt="Blotkake (strawberry and cream sponge cake) from Svalbard"
+          />
           <p className="legend">🇦🇸 Bløtkake from Svalbard</p>
         </div>
         <div>
-          <img src="/mg-kobo-akondo.jpg" alt="koba akondro" />
+          <img
+            src="/mg-kobo-akondo.jpg"
+            alt="Koba Akondro (banana and peanut pudding) from Madagascar"
+          />
           <p className="legend">🇲🇬Koba Akondro from Madagascar</p>
         </div>
         <div>
-          <img src="/mt-kwarezimal.jpg" alt="kwarezimal" />
+          <img
+            src="/mt-kwarezimal.jpg"
+            alt="Kwarezimal (orange blossom biscuits) from Malta"
+          />
           <p className="legend">🇲🇹 Kwareżimal from Malta</p>
         </div>
       </Carousel>
