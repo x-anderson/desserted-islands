3c6a3715fab06cd961b4cba2f0ff37e7f02c17f5
Xander Anderson <x.anderson@synthace.com>
2024-04-28 17:25:20 +0100

    Adjust social icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,17 +2,15 @@ import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
+const icon = faInstagram;
+
 export default function Footer() {
   return (
     <footer className="app-footer">
-      <div className="social-links">
-        <p>👋 I post all my delicious desserts on Instagram here:</p>
+      <div className="links">
+        <p>👋 I post all my delicious desserts here:</p>
         <a href="https://www.instagram.com/desserted_islands/">
-          <FontAwesomeIcon
-            className="social-icon"
-            icon={faInstagram}
-            title="Instagram link"
-          />
+          <FontAwesomeIcon className="icon" icon={icon} title="Link" />
         </a>
       </div>
     </footer>
