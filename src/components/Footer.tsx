1ac599b6b343105ba706dcc45df83b31aadb9429
Xander Anderson <16016467+x-anderson@users.noreply.github.com>
2024-12-18 14:09:04 +0000

    Restyle 3/n - Update icons and Header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,13 @@ import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-const icon = faInstagram;
-
 export default function Footer() {
   return (
     <footer className="app-footer">
       <div className="links">
         <p>👋 I post all my delicious desserts here:</p>
         <a href="https://www.instagram.com/desserted_islands/">
-          <FontAwesomeIcon className="icon" icon={icon} title="Link" />
+          <FontAwesomeIcon className="icon" icon={faInstagram} title="Link" />
         </a>
       </div>
     </footer>
